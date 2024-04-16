faacf2a389ed6596d0ca6f2025ddc52e46b016c0
clockwood8 <clockwoo@udel.edu>
2024-04-16 15:05:26 -0400

    BSTNode

--- BSTNode.cpp
+++ BSTNode.cpp
@@ -1,4 +1,4 @@
-
+//Connor Lockwood and Thomas Boston
 
 #include <iostream>
 #include <string>
@@ -6,10 +6,18 @@
 using namespace std;
 
 BSTNode::BSTNode (string t, string auth, int yr, string pub, long long isbn, float rate) {
-	
+	left = NULL;
+    right = NULL;
+    parent = NULL;
+    book = new Book(t, auth, yr, pub, isbn, rate);
+    height = 0;
 }
 BSTNode::BSTNode() {
-	
+	left = NULL;
+    right = NULL;
+    parent = NULL;
+    book = NULL;
+    height = 0;
 }
 
 BSTNode::~BSTNode(){
